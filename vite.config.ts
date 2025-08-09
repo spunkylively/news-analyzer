9e9b94401dd7c6cd826d8e302e0dcd04a6f46f07
Souta Long <soutalong@slng.local>
2025-08-09 14:59:26 +0900

    refactor: Implement serverless function to fetch news

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // .env.local ファイルから環境変数を読み込む
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: 'https://newsapi.org',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-          // NewsAPIが必要とするヘッダー情報を追加
-          configure: (proxy, _options) => {
-            proxy.on('proxyReq', (proxyReq, _req, _res) => {
-              // 読み込んだ環境変数からAPIキーを設定する
-              proxyReq.setHeader('x-api-key', env.VITE_NEWS_API_KEY);
-            });
-          },
-        },
-      }
-    }
-  }
+export default defineConfig({
+  plugins: [react()],
 })
